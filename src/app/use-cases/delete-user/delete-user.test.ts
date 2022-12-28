312d566cea71c046bc5db88040db0479c193d5a3
Vélmer Oliveira <oliveira.velmer@gmail.com>
2022-12-28 11:20:24 -0300

    delete-user tests

--- src/app/use-cases/delete-user/delete-user.test.ts
+++ src/app/use-cases/delete-user/delete-user.test.ts
@@ -1,1 +1,41 @@
-export {};
+/* eslint-disable jest/no-conditional-expect */
+import { DeleteUser } from ".";
+import { User } from "../../../entities/user";
+import UserRepositoryInMemory from "../../../externals/repositories/in-memory/user-repository-in-memory";
+import { IUserRepository } from "../../contracts/i-user-repository";
+import { CreateUser } from "../create-user";
+
+describe("DeleteUser", () => {
+  let userRepository: IUserRepository;
+
+  beforeAll(() => {
+    userRepository = new UserRepositoryInMemory();
+  });
+
+  it("deletes a user", async () => {
+    const user = {
+      name: "Testevaldo Silva",
+      email: "testevaldo@gmail.com",
+    } as User;
+
+    await CreateUser.execute({ user, userRepository });
+
+    const persistedUser = await DeleteUser.execute({
+      userId: 1,
+      userRepository,
+    });
+
+    expect(persistedUser).toBeUndefined();
+  });
+
+  it("throws an error when user does not exist", async () => {
+    try {
+      await DeleteUser.execute({
+        userId: 1,
+        userRepository,
+      });
+    } catch (error: any) {
+      expect(error.message).toEqual("User does not exist");
+    }
+  });
+});
--- src/app/use-cases/delete-user/delete-user.ts
+++ src/app/use-cases/delete-user/delete-user.ts
@@ -1,14 +1,21 @@
 import { IUserRepository } from "../../contracts/i-user-repository";
+import GetUserById from "../get-user-by-id";
 import { IUseCase } from "../i-use-case";
 
 class DeleteUser implements IUseCase {
-  execute({
+  async execute({
     userId,
     userRepository,
   }: {
     userId: number;
     userRepository: IUserRepository;
   }): Promise<void> {
+    const persistedUser = await GetUserById.execute({ userId, userRepository });
+
+    if (!persistedUser) {
+      throw new Error("User does not exist");
+    }
+
     return userRepository.delete(userId);
   }
 }
