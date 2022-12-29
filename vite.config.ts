9f2d739c221359541268398b7c1df337c1c5589b
Vélmer Oliveira <oliveira.velmer@gmail.com>
2022-12-29 09:50:47 -0300

    switch from Jest to Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     open: "/",
+  },
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
 });
